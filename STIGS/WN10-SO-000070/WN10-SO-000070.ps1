feece233341c29d6090c02e9788a74a0a60c0310
erickcisneros1 <erickcr947@proton.me>
2025-04-30 11:54:59 -0400

    Create WN10-SO-000070.ps1

---
+++ STIGS/WN10-SO-000070/WN10-SO-000070.ps1
@@ -0,0 +1,52 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Erick Cisneros Ruballos
+    LinkedIn        : https://www.linkedin.com/in/erickcr1/
+    GitHub          : https://github.com/erickcisneros1
+    Date Created    : 2025-04-27
+    Last Modified   : 2025-04-27
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000070
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-SO-000070.ps1 
+#>
+
+
+# Define primary registry path and value
+$lsaPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa"
+$lsaValueName = "InactiveThreshold"
+$desiredTimeout = 900
+
+# Create the key if it doesn't exist
+if (-not (Test-Path $lsaPath)) {
+    New-Item -Path $lsaPath -Force | Out-Null
+}
+
+# Set the inactivity timeout
+Set-ItemProperty -Path $lsaPath -Name $lsaValueName -Value $desiredTimeout -Type DWord
+
+# Also set InactivityTimeoutSecs under Winlogon if applicable (used on some builds)
+$winlogonPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$winlogonValue = "InactivityTimeoutSecs"
+
+if (-not (Test-Path $winlogonPath)) {
+    New-Item -Path $winlogonPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $winlogonPath -Name $winlogonValue -Value $desiredTimeout -Type DWord
+
+Write-Host "'Interactive logon: Machine inactivity limit' successfully set to 900 seconds (15 minutes)."
