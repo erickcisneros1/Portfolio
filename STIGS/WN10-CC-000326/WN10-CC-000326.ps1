cbe25727ea89629619807068287592052deefe3d
erickcisneros1 <erickcr947@proton.me>
2025-04-30 11:49:27 -0400

    Update WN10-CC-000326.ps1

--- STIGS/WN10-CC-000326/WN10-CC-000326.ps1
+++ STIGS/WN10-CC-000326/WN10-CC-000326.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+     PowerShell Script Block Logging must be enabled to capture potentially harmful or unauthorized script activity.
 
 .NOTES
     Author          : Erick Cisneros Ruballos
