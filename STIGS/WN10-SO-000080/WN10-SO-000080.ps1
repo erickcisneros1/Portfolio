3b95a3c83e3ad234c8a9bf14b15f1f5f7a0bbe88
erickcisneros1 <erickcr947@proton.me>
2025-04-30 11:57:14 -0400

    Create WN10-SO-000080.ps1

---
+++ STIGS/WN10-SO-000080/WN10-SO-000080.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Erick Cisneros Ruballos
+    LinkedIn        : https://www.linkedin.com/in/erickcr1/
+    GitHub          : https://github.com/erickcisneros1
+    Date Created    : 2025-04-27
+    Last Modified   : 2025-04-27
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000080
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-SO-000080.ps1 
+#>
+
+
+# Set the legal notice caption (title shown on the login screen)
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$regName = "LegalNoticeCaption"
+$regValue = "DoD Notice and Consent Banner"
+
+# Create key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the policy value
+Set-ItemProperty -Path $regPath -Name $regName -Value $regValue -Type String
+
+Write-Host "'Interactive logon: Message title' successfully set to: '$regValue'"
