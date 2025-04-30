3accbd8166adf0a8508e82022853ee06e98d46dc
erickcisneros1 <erickcr947@proton.me>
2025-04-30 11:33:40 -0400

    Create WN10-CC-000326.ps1

---
+++ STIGS/WN10-CC-000326/WN10-CC-000326.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Erick Cisneros Ruballos
+    LinkedIn        : https://www.linkedin.com/in/erickcr1/
+    GitHub          : https://github.com/erickcisneros1
+    Date Created    : 2025-04-27
+    Last Modified   : 2025-04-27
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000326
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000326.ps1 
+#>
+
+
+# Define registry path and values
+$regPath = "HKLM:\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"
+$regName = "EnableScriptBlockLogging"
+$regValue = 1
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the policy value
+Set-ItemProperty -Path $regPath -Name $regName -Value $regValue -Type DWord
+
+Write-Host "'Turn on PowerShell Script Block Logging' policy set to 'Enabled'."
