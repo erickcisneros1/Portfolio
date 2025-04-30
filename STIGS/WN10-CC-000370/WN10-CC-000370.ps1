b0036fd2914f8444eb43b9ff103fe860c49549bd
erickcisneros1 <erickcr947@proton.me>
2025-04-30 11:52:54 -0400

    Create WN10-CC-000370.ps1

---
+++ STIGS/WN10-CC-000370/WN10-CC-000370.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Erick Cisneros Ruballos
+    LinkedIn        : https://www.linkedin.com/in/erickcr1/
+    GitHub          : https://github.com/erickcisneros1
+    Date Created    : 2025-04-27
+    Last Modified   : 2025-04-27
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000370
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000370.ps1 
+#>
+
+
+# Define the registry path and values
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System"
+$regName = "AllowDomainPINLogon"
+$regValue = 0
+
+# Create the key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Apply the setting
+Set-ItemProperty -Path $regPath -Name $regName -Value $regValue -Type DWord
+
+Write-Host "'Turn on convenience PIN sign-in' policy set to 'Disabled' successfully."
