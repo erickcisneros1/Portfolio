dd7786f551711eefa2d162cc54496a32d239d5d1
erickcisneros1 <erickcr947@proton.me>
2025-04-30 11:17:27 -0400

    Add files via upload

---
+++ STIGS/WN10-AU-000035/WN10-AU-000035.ps1
@@ -0,0 +1,43 @@
+﻿<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Erick Cisneros Ruballos
+    LinkedIn        : https://www.linkedin.com/in/erickcr1/
+    GitHub          : https://github.com/erickcisneros1
+    Date Created    : 2025-04-27
+    Last Modified   : 2025-04-27
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000035
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-AU-000035.ps1 
+#>
+
+
+# Ensure script is run as Administrator
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "This script must be run as Administrator."
+    exit 1
+}
+
+# Configure the audit policy for User Account Management (Failure only)
+$auditCategory = "Account Management"
+$auditSubcategory = "User Account Management"
+
+# Use AuditPol to configure failure auditing
+$auditCommand = "auditpol /set /subcategory:`"$auditSubcategory`" /failure:enable"
+Invoke-Expression $auditCommand
+
+Write-Host "'Audit User Account Management' policy set to log Failure events."
